d2a5c2fc9e6397fb5e87f6666d0c48ffc8fa61e5
watsonzer <b.jaime@orange.fr>
2025-04-04 10:37:35 +0200

    first try

--- src/main/java/fr/isep/algo/Employe.java
+++ src/main/java/fr/isep/algo/Employe.java
@@ -1,4 +1,33 @@
 package fr.isep.algo;
 
-public class Employe {
+import java.util.Date;
+
+public abstract class Employe extends Personne {
+    protected String poste;
+    protected double salaire;
+    protected Date dateEmbauche;
+
+    public Employe(String id, String nom, String prenom, String adresse, String telephone,
+                   String email, String poste, double salaire, Date dateEmbauche) {
+        super(id, nom, prenom, adresse, telephone, email);
+        this.poste = poste;
+        this.salaire = salaire;
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    public String getPoste() { return poste; }
+    public void setPoste(String poste) { this.poste = poste; }
+
+    public double getSalaire() { return salaire; }
+    public void setSalaire(double salaire) { this.salaire = salaire; }
+
+    public Date getDateEmbauche() { return dateEmbauche; }
+    public void setDateEmbauche(Date dateEmbauche) { this.dateEmbauche = dateEmbauche; }
+
+    public abstract String obtenirRole();
+
+    @Override
+    public String obtenirInfos() {
+        return "Employé: " + id + ", " + nom + " " + prenom + ", Poste: " + poste;
+    }
 }
--- src/main/java/fr/isep/algo/Main.java
+++ src/main/java/fr/isep/algo/Main.java
@@ -1,17 +1,122 @@
 package fr.isep.algo;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
+/**
+ * Classe principale pour démarrer l'application de gestion de compagnie aérienne
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome to the world!");
+        // Création d'une compagnie aérienne
+        CompagnieAerienne airFrance = new CompagnieAerienne("Air France", "AF");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Création d'avions
+        Avion avion1 = new Avion("F-GKXL", "Airbus A320", 180);
+        Avion avion2 = new Avion("F-HPJE", "Boeing 787", 300);
+
+        airFrance.ajouterAvion(avion1);
+        airFrance.ajouterAvion(avion2);
+
+        // Création d'employés
+        Pilote pilote1 = new Pilote("P001", "Dubois", "Jean", "1 Rue Paris", "0123456789",
+                "jean.dubois@airfrance.fr", "Commandant de bord", 80000.0,
+                new Date(), "PLCM001", 5000);
+        pilote1.ajouterQualification("A320");
+
+        PersonnelCabine hotesse1 = new PersonnelCabine("PC001", "Martin", "Sophie", "2 Rue Lyon",
+                "0987654321", "sophie.martin@airfrance.fr",
+                "Chef de cabine", 45000.0, new Date(), "CERT001");
+        hotesse1.ajouterLangue("Français");
+        hotesse1.ajouterLangue("Anglais");
+
+        airFrance.ajouterEmploye(pilote1);
+        airFrance.ajouterEmploye(hotesse1);
+
+        // Création d'un vol
+        // Supposons que nous sommes en décembre 2024
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2024, Calendar.DECEMBER, 10, 8, 0); // 10 décembre 2024, 8h00
+        Date dateDepart = calendar.getTime();
+
+        calendar.set(2024, Calendar.DECEMBER, 10, 10, 30); // 10 décembre 2024, 10h30
+        Date dateArrivee = calendar.getTime();
+
+        Vol vol1 = new Vol("AF1234", "Paris", "Nice", dateDepart, dateArrivee);
+        vol1.setAvion(avion1);
+
+        // Affecter l'équipage
+        List<Employe> equipage = new ArrayList<>();
+        equipage.add(pilote1);
+        equipage.add(hotesse1);
+        vol1.affecterEquipage(equipage);
+
+        airFrance.planifierVol(vol1);
+
+        // Création d'un passager et réservation
+        Passager passager1 = new Passager("C001", "Petit", "Marie", "3 Rue Marseille",
+                "0654321987", "marie.petit@gmail.com", "PAB123456", "Française");
+
+        airFrance.ajouterPassager(passager1);
+
+        Reservation reservation1 = passager1.reserverVol(vol1, "Économique", 150.0);
+
+        // Affichage des informations
+        System.out.println("=== SYSTÈME DE GESTION DE COMPAGNIE AÉRIENNE ===");
+        System.out.println("\nDétails du vol:");
+        System.out.println(vol1.obtenirVol());
+
+        System.out.println("\nDétails de la réservation:");
+        System.out.println(reservation1.obtenirReservations());
+
+        System.out.println("\nInformations du passager:");
+        System.out.println(passager1.obtenirInfos());
+
+        System.out.println("\nInformations du pilote:");
+        System.out.println(pilote1.obtenirInfos());
+
+        System.out.println("\nInformations du personnel de cabine:");
+        System.out.println(hotesse1.obtenirInfos());
+
+        // Démonstration de l'annulation d'une réservation
+        System.out.println("\n=== ANNULATION DE RÉSERVATION ===");
+        boolean annulationReussie = passager1.annulerReservation(reservation1.getNumeroReservation());
+        System.out.println("Annulation de la réservation " + reservation1.getNumeroReservation() +
+                ": " + (annulationReussie ? "Réussie" : "Échouée"));
+
+        // Création d'un second vol
+        calendar.set(2024, Calendar.DECEMBER, 15, 14, 0); // 15 décembre 2024, 14h00
+        Date dateDepart2 = calendar.getTime();
+
+        calendar.set(2024, Calendar.DECEMBER, 15, 16, 0); // 15 décembre 2024, 16h00
+        Date dateArrivee2 = calendar.getTime();
+
+        Vol vol2 = new Vol("AF5678", "Paris", "Lyon", dateDepart2, dateArrivee2);
+        vol2.setAvion(avion2);
+        vol2.affecterEquipage(equipage);
+
+        airFrance.planifierVol(vol2);
+
+        // Nouvelle réservation
+        Reservation reservation2 = passager1.reserverVol(vol2, "Affaires", 250.0);
+
+        // Démonstration d'annulation de vol
+        System.out.println("\n=== ANNULATION DE VOL ===");
+        boolean volAnnule = airFrance.annulerVol(vol2.getNumeroVol());
+        System.out.println("Annulation du vol " + vol2.getNumeroVol() +
+                ": " + (volAnnule ? "Réussie" : "Échouée"));
+
+        // Vérification du statut après annulation
+        System.out.println("Statut du vol après annulation: " + vol2.getStatut());
+        System.out.println("Statut de la réservation après annulation: " + reservation2.getStatut());
+
+        // Statistiques (bonus)
+        System.out.println("\n=== STATISTIQUES ===");
+        System.out.println("Revenu total: " + airFrance.calculerRevenusTotal() + " €");
+
+        Map<String, Integer> destinationsPopulaires = airFrance.statistiquesDestinationsPopulaires();
+        System.out.println("\nDestinations populaires:");
+        for (Map.Entry<String, Integer> entry : destinationsPopulaires.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " passagers");
         }
     }
 }
