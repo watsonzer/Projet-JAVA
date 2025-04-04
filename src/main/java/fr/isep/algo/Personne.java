c7f8c944d4c367599a5f6f8672847035e9b6c0b2
watsonzer <b.jaime@orange.fr>
2025-04-04 10:37:35 +0200

    first try

--- src/main/java/fr/isep/algo/Personne.java
+++ src/main/java/fr/isep/algo/Personne.java
@@ -1,4 +1,42 @@
 package fr.isep.algo;
 
-public class Personne {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Personne {
+    protected String id;
+    protected String nom;
+    protected String prenom;
+    protected String adresse;
+    protected String telephone;
+    protected String email;
+
+    public Personne(String id, String nom, String prenom, String adresse, String telephone, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    public String getAdresse() { return adresse; }
+    public void setAdresse(String adresse) { this.adresse = adresse; }
+
+    public String getTelephone() { return telephone; }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public abstract String obtenirInfos();
 }
--- src/main/java/fr/isep/algo/Pilote.java
+++ src/main/java/fr/isep/algo/Pilote.java
@@ -1,4 +1,41 @@
 package fr.isep.algo;
 
-public class Pilote {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Pilote extends Employe {
+    private String licenceNum;
+    private int heuresVol;
+    private List<String> qualifications;
+
+    public Pilote(String id, String nom, String prenom, String adresse, String telephone,
+                  String email, String poste, double salaire, Date dateEmbauche,
+                  String licenceNum, int heuresVol) {
+        super(id, nom, prenom, adresse, telephone, email, poste, salaire, dateEmbauche);
+        this.licenceNum = licenceNum;
+        this.heuresVol = heuresVol;
+        this.qualifications = new ArrayList<>();
+    }
+
+    public String getLicenceNum() { return licenceNum; }
+    public void setLicenceNum(String licenceNum) { this.licenceNum = licenceNum; }
+
+    public int getHeuresVol() { return heuresVol; }
+    public void setHeuresVol(int heuresVol) { this.heuresVol = heuresVol; }
+
+    public List<String> getQualifications() { return qualifications; }
+    public void ajouterQualification(String qualification) {
+        this.qualifications.add(qualification);
+    }
+
+    @Override
+    public String obtenirRole() {
+        return "Pilote";
+    }
+
+    @Override
+    public String obtenirInfos() {
+        return super.obtenirInfos() + ", Licence: " + licenceNum + ", Heures de vol: " + heuresVol;
+    }
 }
